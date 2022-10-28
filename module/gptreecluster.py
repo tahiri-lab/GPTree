f7f51521856e237e97ec019d135b9ce6f76070bb
Alex <16609593+cmapman@users.noreply.github.com>
2022-10-28 10:35:29 -0400

    Update subprocess

--- module/gptreecluster.py
+++ module/gptreecluster.py
@@ -27,7 +27,7 @@ import subprocess
 
 # implement pip as a subprocess:
 package = ['pandas', 'ete3', 'PyQt5', 'asymmetree']
-subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + package)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install'] + package, stdout=subprocess.DEVNULL)
 
 # process output with an API in the subprocess module:
 reqs = subprocess.check_output([sys.executable, '-m', 'pip'])
